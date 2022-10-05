767c5c805290292230e93a8f7c20a24dcb725f53
Gopal Anand <67302165+gopal0208@users.noreply.github.com>
2022-10-05 14:23:11 +0530

    Create Category.cs

---
+++ mvc/ProductApi/Models/Category.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+
+namespace ProductApi.Models;
+
+public partial class Category
+{
+    public int CategoryId { get; set; }
+
+    public string CategoryName { get; set; } = null!;
+
+    public virtual ICollection<Product> Products { get; } = new List<Product>();
+}
