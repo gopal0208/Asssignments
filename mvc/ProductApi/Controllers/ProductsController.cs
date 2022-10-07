fd799dce01347b85b0b4a38c8b11c34837b40889
Gopal Anand <67302165+gopal0208@users.noreply.github.com>
2022-10-07 09:59:01 +0530

    Update ProductsController.cs

--- mvc/ProductApi/Controllers/ProductsController.cs
+++ mvc/ProductApi/Controllers/ProductsController.cs
@@ -28,52 +28,9 @@ public class ProductsController : ControllerBase
         return Ok(prod);
     }
 
-    
-    [HttpDelete("Remove/(id)")]
-    public IActionResult Remove(int id)
-    {
-        var prod=this._DBContext.Products.FirstOrDefault(o=>o.Id==id);
-        if(prod!=null){
-            this._DBContext.Remove(prod);
-            this._DBContext.SaveChanges();
-            return Ok(true);
-        }
-        return Ok(false);
-    }
-
-    
-    [HttpPost("Create")]
-    public IActionResult Create([FromBody] Product _product)
-    {
-        var prod=this._DBContext.Products.FirstOrDefault(o=>o.Id==_product.Id);
-        if(prod!=null){
-            prod.Brand=_product.Brand;
-            prod.CategoryId=_product.CategoryId;
-            prod.Descrip=_product.Descrip;
-            prod.DiscountPercentage=_product.DiscountPercentage;
-            prod.Price=_product.Price;
-            prod.Rating=_product.Rating;
-            prod.Thumbnail=_product.Thumbnail;
-            prod.Stock=_product.Stock;
-            prod.Title=_product.Title;
-            this._DBContext.SaveChanges();
-        }
-        else{
-            this._DBContext.Products.Add(_product);
-            this._DBContext.SaveChanges();
-        }
-        return Ok(true);
-    }
-
-    [HttpGet("GetByPrice/(minimumPrice,maximumPrice)")]
-    public IActionResult getByPrice(int mininmumPrice,int maximumPrice){
-        var prod = this._DBContext.Products.Where(o=>(o.Price>=mininmumPrice && o.Price<=maximumPrice));
-        return Ok(prod);
-    }
-
-    [HttpGet("GetBybrand/(brandName)")]
-    public IActionResult getByBrand(string brandName){
-        var prod= this._DBContext.Products.Where(o=>o.Brand==brandName);
+    [HttpGet("GetByPrice/(minimumPrice,maximumPrice,brandName)")]
+    public IActionResult getByPrice(int mininmumPrice,int maximumPrice,string brandName){
+        var prod = this._DBContext.Products.Where(o=>(o.Price>=mininmumPrice && o.Price<=maximumPrice && o.Brand==brandName));
         return Ok(prod);
     }
 
